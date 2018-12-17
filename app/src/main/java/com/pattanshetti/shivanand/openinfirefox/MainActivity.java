b7a1c36f13b74c1a3ed90c494d451fe6a8e51670
shivanand.pattanshetti <shivanand.pattanshetti@gmail.com>
2018-12-17 02:38:25 -0600

    .
    Signed-off-by: shivanand.pattanshetti <shivanand.pattanshetti@gmail.com>

--- app/src/main/java/com/pattanshetti/shivanand/openinfirefox/MainActivity.java
+++ app/src/main/java/com/pattanshetti/shivanand/openinfirefox/MainActivity.java
@@ -2,6 +2,9 @@ package com.pattanshetti.shivanand.openinfirefox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.os.Environment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        try {
+            Log.d("CUSTOM:Intent", intent.toString());
+            Log.d("CUSTOM:Action", action.toString());
+            Log.d("CUSTOM:Type", type.toString());
+        }catch(Exception e){
+        }
+        if (Intent.ACTION_VIEW.equals(action) && type != null) {
+            if ("text/html".equals(type)) {
+                openInFirefox(intent);
+            }
+        }
+    }
+
+    void openInFirefox(Intent intent){
+        String URI = intent.getData().getPath().replace("/external_files/", "file://"+Environment.getExternalStorageDirectory().getPath()+"/");
     }
 }

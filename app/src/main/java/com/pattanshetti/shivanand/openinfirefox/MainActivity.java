98d46da83596a42a6cea7c4c24b92b5dbb098e26
shivanand.pattanshetti <shivanand.pattanshetti@gmail.com>
2018-12-17 23:29:17 -0600

    ...
    Signed-off-by: shivanand.pattanshetti <shivanand.pattanshetti@gmail.com>

--- app/src/main/java/com/pattanshetti/shivanand/openinfirefox/MainActivity.java
+++ app/src/main/java/com/pattanshetti/shivanand/openinfirefox/MainActivity.java
@@ -1,14 +1,25 @@
 package com.pattanshetti.shivanand.openinfirefox;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
-import android.os.*;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.os.Environment;
-import java.lang.reflect.*;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
+    // https://stackoverflow.com/questions/3976616/how-to-find-nth-occurrence-of-character-in-a-string
+    public static int ordinalIndexOf(String str, String substr, int n) {
+        int pos = str.indexOf(substr);
+        while (--n > 0 && pos != -1)
+            pos = str.indexOf(substr, pos + 1);
+        return pos;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,35 +28,41 @@ public class MainActivity extends AppCompatActivity {
 
         // Get intent, action and MIME type
         Intent intent = getIntent();
+        Context context = getApplicationContext();
         String action = intent.getAction();
         String type = intent.getType();
         try {
             Log.d("CUSTOM:Intent", intent.toString());
             Log.d("CUSTOM:Action", action.toString());
             Log.d("CUSTOM:Type", type.toString());
-        }catch(Exception e){
+        } catch (Exception e) {
         }
         if (Intent.ACTION_VIEW.equals(action) && type != null) {
             if ("text/html".equals(type)) {
-                openInFirefox(intent);
+                openInFirefox(context, intent);
             }
         }
     }
 
-    void openInFirefox(Intent intent){
-        if(Build.VERSION.SDK_INT>=24){
-            try{
+    void openInFirefox(Context context, Intent intent) {
+        if (Build.VERSION.SDK_INT >= 24) {
+            try {
                 Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
                 m.invoke(null);
-            }catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        String URI = intent.getData().getPath().replace("/external_files/", "file://"+Environment.getExternalStorageDirectory().getPath()+"/");
+        Log.d("CUSTOM:PathforURI", intent.getData().getPath());
+        String URI = intent.getData().getPath();
+        URI = URI.substring(ordinalIndexOf(URI, "/", 2) + 1);
+        URI = "file://"/*+Environment.getExternalStorageDirectory().getPath()*/ + "/" + URI;
+        Log.d("CUSTOM:URI", URI);
         Intent firefoxIntent = new Intent(intent.getAction());
         firefoxIntent.setDataAndType(Uri.parse(URI), intent.getType());
         // startActivity(Intent.createChooser(firefoxIntent, "Open file using:"));
         firefoxIntent.setPackage("org.mozilla.firefox");
+
         startActivity(firefoxIntent);
         this.finish();
     }
---
+++ app/src/main/java/com/pattanshetti/shivanand/openinfirefox/PathDecoder.java
@@ -0,0 +1,131 @@
+package com.pattanshetti.shivanand.openinfirefox;
+
+import android.net.Uri;
+
+public class PathDecoder {
+    private String scheme;
+    private int schemeTokenIndex;
+    private String host;
+    private int hostTokenIndex;
+    private String path;
+    private String name;
+    private int nameTokenIndex;
+    private String extension;
+    private boolean nullState;
+
+    public void PathDecoder() {
+        scheme = "";
+        schemeTokenIndex = -1;
+        host = "";
+        hostTokenIndex = -1;
+        path = "";
+        name = "";
+        nameTokenIndex = -1;
+        extension = "";
+        nullState = true;
+    }
+
+    public String getScheme() {
+        return scheme;
+    }
+
+    public void setScheme(String scheme) {
+        this.scheme = scheme;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public boolean isNull() {
+        return nullState;
+    }
+
+    public String getPathFromURI(Uri URI) {
+        if (URI == null) {
+            return "";
+        }
+        nullState = false;
+
+        String parsedPath = "";
+
+        parsedPath = getPathFromDecodedString(URI.getPath());
+
+        path = parsedPath;
+        return parsedPath;
+    }
+
+    public String getPathFromURIString(String URIString) {
+        if (URIString == null || URIString.isEmpty()) {
+            return "";
+        }
+        nullState = false;
+
+        String parsedPath = "";
+        String decodedPath = "";
+        String[] tokens = URIString.split("/");
+        for (int i = 0; i < tokens.length; i++) {
+            String token = tokens[i];
+            if (token.isEmpty()) {
+                continue;
+            } else if (token.indexOf(':') == token.length() - 1) {
+                // Scheme string
+                scheme = token.substring(0, token.length() - 1);
+                schemeTokenIndex = i;
+            } else if (i > 0 && tokens[i - 1].isEmpty()) {
+                // Host string
+                host = token;
+                hostTokenIndex = i;
+            } else if (i == tokens.length - 1) {
+                // Name string
+                name = token;
+                nameTokenIndex = i;
+                extension = name.substring(name.lastIndexOf('.') + 1);
+                decodedPath += "/" + token;
+            } else {
+                decodedPath += "/" + token;
+            }
+        }
+
+        parsedPath = getPathFromDecodedString(decodedPath);
+
+        path = parsedPath;
+        return parsedPath;
+    }
+
+    public String getPathFromDecodedString(String decodedString) {
+        String parsedPath = "";
+
+
+
+        return parsedPath;
+    }
+}
